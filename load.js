a38c99d95ae0f5c2129920016e42de63ed01e818
Manumental32 <manuel.aquino.utn@gmail.com>
2019-04-12 01:30:18 -0300

    rm notifications

--- load.js
+++ load.js
@@ -3,13 +3,16 @@ function changeStatus(value) {
 
   if(value == "true") {
     body.className = 'busy';
-    body.innerHTML = 'Cagaste! Esta ocupado!';
+    body.innerHTML = 'Ocupado!';
+    //notifyMe('Ocupado!');
   } else {
     body.className = 'free';
     body.innerHTML = 'Está libre campeón!';
-    var audio = new Audio('cool.ogg');
-    audio.play();
+    //notifyMe('Está libre campeón!');
+    //var audio = new Audio('cool.ogg');
+    //audio.play();
   }
+  
 }
 
 
@@ -17,15 +20,48 @@ function load() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      changeStatus(this.response)
+      changeStatus(this.response);
     }
   };
-  xhttp.open("GET", "https://enigmatic-caverns-58480.herokuapp.com/", true);
+  xhttp.open("GET", "https://troca-banio.herokuapp.com/", true);
   xhttp.send();
 }
 
-window.onload = load;
+var loadInterval;
 
+function checkStatus() {
+  loadInterval = setInterval(function(){ load(); }, 10000);
+}
+
+window.onload = checkStatus();
+
+//clearInterval(loadInterval);
+/* 
+function notifyMe(msg) {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support system notifications");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(msg);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(msg);
+      }
+    });
+  }
+ */
+  // Finally, if the user has denied notifications and you 
+  // want to be respectful there is no need to bother them any more.
+//}
 
 // $(function() {
 
